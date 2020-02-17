2b569ffbeee22dccd2b2d6225b09af268296dd40
Simon Edvardsson <Simon.Edvards@gmail.com>
2020-02-17 17:43:04 +0100

    added the first working version of our DSL

--- haskell/first/test_game.hs
+++ haskell/first/test_game.hs
@@ -3,29 +3,32 @@ import Game
 
 main :: IO ()
 main = do
-    -- variables stuff
-    let preds = predicates ["turn"
-                           ,"rock"
-                           ,"paper"
-                           ,"scissors"
-                           ,"win"
-                           ,"lose"
-                           ,"token"]
-    write preds "game.cep"
-
     -- Variable stuff
+    let t = createType ["player"]
+    write t "game.cep"
+
     let prods = prodicates "player" ["turn"
-                                         ,"rock"
-                                         ,"paper"
-                                         ,"scissors"
-                                         ,"win"
-                                         ,"lose"
-                                         ,"token"]
+                                    ,"rock"
+                                    ,"paper"
+                                    ,"scissor"
+                                    ,"win"
+                                    ,"lose"
+                                    ,"token"]
     write prods "game.cep"
 
     let move = moves "game" ["rock","paper","scissor"]
     write move "game.cep"
 
-    -- 
+    let win = winCondition ["rock beats scissor"
+                           ,"paper beats rock"
+                           ,"scissor beats paper"
+                           ,"rock draws rock"
+                           ,"paper draws paper"
+                           ,"scissor draws scissor"]
+    write win "game.cep"
 
+    let players = generateTurn ["jennie", "peter"]
+    write players "game.cep"
+
+    write Trace "game.cep" 
 
