432c227c6d9995ebbc75f06a0effee71a861a28d
Simon Edvardsson <Simon.Edvards@gmail.com>
2020-02-17 09:08:54 +0100

    Added som files for first version of dsl

---
+++ haskell/first/test_game.hs
@@ -0,0 +1,29 @@
+import Game
+
+
+main :: IO ()
+main = do
+    -- variables stuff
+    let preds = predicates ["turn"
+                           ,"rock"
+                           ,"paper"
+                           ,"scissors"
+                           ,"win"
+                           ,"lose"
+                           ,"token"]
+    write preds "game.cep"
+
+    -- Variable stuff
+    let prods = prodicates "player" ["turn"
+                                         ,"rock"
+                                         ,"paper"
+                                         ,"scissors"
+                                         ,"win"
+                                         ,"lose"
+                                         ,"token"]
+    write prods "game.cep"
+
+    -- 
+
+
+
