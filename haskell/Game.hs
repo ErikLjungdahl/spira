ac220280a691a0b36ec1b0816597e8d1ccb857de
ErikLjungdahl <ekke.ekke@gmail.com>
2020-02-24 11:05:01 +0100

    Beginning of tic-tac-toe implementation, done together with Simon and Jennie

--- haskell/Game.hs
+++ haskell/Game.hs
@@ -14,11 +14,11 @@ import Control.Monad.Writer
 -- Datatype for creating single predicates
 data Game = Spred [String]
           | Type [String]
-          | Tpred String [String]
+          | Tpred [String] [String] String
           | Move [String]
           | Rule [String]
           | StageInteractive String Game
-          | Stage String Game
+          | Stage String [String]
           | Turn [String]
           | Trace String
           | And Game Game
@@ -42,25 +42,28 @@ runGame g fp = do
 g1 & g2 = g1 `And` g2
 
 
--- Creates single predicates
-predicates :: [String] -> Game
-predicates xs = Spred xs
-
 --Create types
-createType :: [String] -> Game
-createType xs = Type xs
+types :: [String] -> Game
+types xs = Type xs
+
+preds :: [String] -> Game
+preds = predicates []
 
 -- Creates predicates with a type
-prodicates :: String -> [String] -> Game
-prodicates t preds = Tpred t preds
+predicates :: [String] -> [String] -> Game
+predicates ts preds = Tpred ts preds "pred"
+
+-- Creates predicates with a type
+typePredicates :: [String] -> [String] -> String -> Game
+typePredicates as preds t = Tpred as preds t
 
 -- Create moves into a StageInteractive
 moves :: String -> [String] -> Game
 moves name xs = StageInteractive name $ Move xs
 
 -- Create rules for win condition
-winCondition :: [String] -> Game
-winCondition xs = Stage "result" $ Rule xs
+winCondition :: String -> [String] -> Game
+winCondition n xs = Stage n $ xs
 
 -- Create rules turnbased system
 generateTurn :: [String] -> Game
@@ -69,24 +72,82 @@ generateTurn xs = Turn xs
 trace :: String -> Game
 trace = Trace
 
+beats :: String -> String -> String
+beats a b = winString a b
+
+draws :: String -> String -> String
+draws a b = drawString a b
+
+
+inARow :: Int -> String
+inARow n =  intercalate "\n\t" combined
+    where
+    begin    = "win/row"
+    token    = ": token B"
+    occupied = ["* occupied A " ++ successor n' "X" ++ " Y" | n' <- [0..n-1]]
+    end      = "-o win A."
+    combined = begin : token : occupied ++ [end]
+
+inAColumn :: Int -> String
+inAColumn n =  intercalate "\n\t" combined
+    where
+    begin    = "win/column"
+    token    = ": token B"
+    occupied = ["* occupied A " ++ "X " ++ successor n' "Y"  | n' <- [0..n-1]]
+    end      = "-o win A."
+    combined = begin : token : occupied ++ [end]
+
+inADiagonal :: Int -> String
+inADiagonal n =  combined
+    where
+    begin    = "win/diagonal/"
+    token    = ": token B"
+    occupiedUp   = ["* occupied A "
+                 ++ successor n' "X" ++ " " ++ successor n' "Y"
+                 | n' <- [0..n-1]]
+    occupiedDown = ["* occupied A "
+                 ++ successor n' "X" ++ " " ++ successor (n-1-n') "Y"
+                 | n' <- [0..n-1]]
+    end      = "-o win A."
+    up   = intercalate "\n\t" $ (begin ++ "up")  : token : occupiedUp   ++ [end]
+    down = intercalate "\n\t" $ (begin ++ "down") : token : occupiedDown ++ [end]
+    combined = up ++ "\n" ++ down
+
+successor :: Int -> String -> String
+successor x s = suc x s
+  where
+    suc 0 s = s
+    suc x s = "(s " ++ suc (x-1) s ++ ")"
+
+allFree :: Int -> String
+allFree x' = "context all_free = {" ++ generate (x'-1) (x'-1) ++ "}"
+  where
+    genStr x y = "\nfree\t" ++ successor x "z" ++ "\t" ++ successor y "z"
+    generate x y
+      | x == 0 && y == 0 = genStr x y
+      | y == 0           = genStr x y ++ generate (x-1) (x'-1)
+      | otherwise        = genStr x y ++ generate (x) (y-1)
+
+
+
+
 
 
 -- Creates the string that are able to write to the file
 createGame :: Game -> M ()
 createGame g =  case g of
-    (Spred [])         -> tell "\n"
-    (Spred (x:xs))     -> do tell $ x ++ " : pred.\n"
-                             createGame (Spred xs)
-    (Tpred t [])       -> tell $ "\n"
-    (Tpred t (x:xs))   -> do tell $ x ++ " " ++ t ++ " : pred.\n"
-                             createGame (Tpred t xs)
-    (Type [])          -> tell $ "\n"
-    (Type (x:xs))      -> do tell (x ++ " : type.\n")
-                             createGame (Type xs)
-    (StageInteractive str game) -> tell $ "stage " ++ str ++ " = {\n"
-                                ++ createString game ++ "}\n#interactive game.\n\n"
-    (Stage str game)   -> tell $ transition ++ "stage " ++ str ++ " = {\n" ++ createRules game ++ "}\n\n"
-    (Turn xs)          -> tell $ createTurn xs
+    Tpred as [] t       -> tell $ "\n"
+    Tpred as (x:xs) t    -> do tell $ x ++ " " ++ (intercalate " " as) ++ " : " ++ t ++ ".\n"
+                               createGame (Tpred as xs t)
+    Type []          -> tell $ "\n"
+    Type (x:xs)      -> do tell (x ++ " : type.\n")
+                           createGame (Type xs)
+    StageInteractive str game -> tell $ "stage " ++ str ++ " = {\n"
+                                      ++ createString game ++ "}\n#interactive game.\n\n"
+    Stage str rules   -> tell $ transition ++ "stage " ++ str ++ " = {\n"
+                              ++ (intercalate "\n" rules)
+                              ++ "\n}\n\n"
+    Turn xs          -> tell $ createTurn xs
     Trace name         -> tell $ "#trace _ " ++ name ++" init."
     And g1 g2 -> do
         createGame g1
@@ -103,13 +164,6 @@ createTurn :: [String] -> String
 createTurn xs = head xs ++ " : player.\n" ++ last xs ++ " : player.\n\n"
     ++ "context init = \n" ++ "{turn " ++ head xs ++ ", turn " ++ last xs ++ "}.\n\n"
 
--- Helper function for creating the rules
-createRules :: Game -> String
-createRules (Rule [])   = ""
-createRules (Rule (x:xs)) = case (word !! 1) -- takes middle word and use it for cases
-    of "beats" -> winString  (head word) (last word) ++ "\n" ++ createRules (Rule xs)
-       "draws" -> drawString (head word) (last word) ++ "\n" ++ createRules (Rule xs)
-       where word = splitOn " " x
 
 -- Helper function for creating the wincondition string
 winString :: String -> String -> String
--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,23 +1,52 @@
 import Game
 
 main :: IO ()
-main = runGame rockPaperScissor "game.cep"
+main = runGame ticTacToe "game.cep"
 
 
 rockPaperScissor :: Game
 rockPaperScissor
-     = createType ["player"]
+     = types ["player"]
      -- TODO extract the predicates from the moves/wincondition?
-     & prodicates "player" ["turn","rock","paper","scissor","win","lose","token"]
+     & predicates ["player"] ["turn","rock","paper","scissor","win","lose","token"]
      & moves "game" ["rock","paper","scissor"]
-     & winCondition ["rock beats scissor"
-                    ,"paper beats rock"
-                    ,"scissor beats paper"
-                    ,"rock draws rock"
-                    ,"paper draws paper"
-                    ,"scissor draws scissor"]
+     & winCondition "result"
+         ["rock"    `beats` "scissor"
+         ,"paper"   `beats` "rock"
+         ,"scissor" `beats` "paper"
+         ,"rock"    `draws` "rock"
+         ,"paper"   `draws` "paper"
+         ,"scissor" `draws` "scissor"]
 
      & generateTurn ["jennie", "peter"]
      & trace "game"
 
      -- TODO "game" must be specificed in both moves and trace
+
+
+ticTacToe :: Game
+ticTacToe
+    = types ["player"]
+    & types ["nat"] -- Remove later?
+    & typePredicates [] ["z"] "nat" -- Remove later?
+    & typePredicates ["nat"] ["s"] "nat"
+    & preds ["draw"]
+    & predicates ["player"] ["turn","token","win"]
+    & typePredicates ["player","player"] ["opp"] "bwd"
+    & predicates ["nat","nat"] ["free","restore"]
+    & predicates ["player","nat","nat"] ["occupied"]
+    & preds ["full","not_full_yet"]
+    -- TODO StageInteractive play
+    & winCondition "win"
+        [ inARow 3
+        , inAColumn 3
+        , inADiagonal 3
+        -- TODO Check/draw
+        ]
+    -- TODO go/play
+    -- TODO go/draw
+    -- TODO stage draw
+    -- TODO Objects
+    -- TODO context allFree
+    -- TODO context init
+    & trace "play"
