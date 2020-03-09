c7ed04fe91fad14ab6c157c084f3d7f680b05245
ErikLjungdahl <ekke.ekke@gmail.com>
2020-03-09 11:23:14 +0100

    Pred is flatter

--- haskell/Data.hs
+++ haskell/Data.hs
@@ -1,28 +1,39 @@
 module Data where
 
-data Game = Stage StagePred [Implication ApplyPred] -- "stage " ++ Stagepred ++ ...
-          | Transition Name (Implication ApplyPredOrStage)
+
+data Game = Stage Name [Implication] -- "stage " ++ Stagepred ++ ...
+          | Transition Name Implication
+          deriving (Show)
      -- | Context Name [Pred] -- Not really needed, since they can only be used in init
 
-data Implication a = Implication [a] [a] -- Basically -o (lolli) between the two lists
+data Implication = Implication [Pred] [Pred] -- Basically -o (lolli) between the two lists
+              deriving (Show)
 
 data Pred = Pred Name [Type]
           | Bwd Name [Type]
+          | StagePred Name
+          | ApplyPred Pred [Pred] -- Pred (1)  -- Pred (2) should be a Var
+          | Var Name Type
+        deriving (Show)
 
-data ApplyPred = ApplyPred Pred [Var]
 
-newtype StagePred = StagePred Name
-
-data ApplyPredOrStage = SP StagePred  -- "stage " ++ Stagepred
-                      | AP ApplyPred
-
-data InitPred = InitPred Pred [Val]
 data Initial = Initial
-                    StagePred -- Initial stage
-                    [InitPred]   -- Initial context.
+                    Name -- StagePred Initial stage
+                    [Pred]   -- Pred that doesn't have Var or bwd -- Initial context.
+          deriving (Show)
 
 type Name = String
-newtype Type = Type Name -- Det som användaren genererar.
+data Type = Type Name -- Det som användaren genererar.
+          deriving (Show)
 
-data Var = Var Name Type
-type Val = String
+-- data Var = Var Name Type
+-- type Val = String
+
+-- data ApplyPred = ApplyPred Pred [Var]
+
+-- newtype StagePred = StagePred Name
+
+-- data ApplyPredOrStage = SP StagePred  -- "stage " ++ Stagepred
+--                       | AP ApplyPred
+
+-- data InitPred = InitPred Pred [Val]
