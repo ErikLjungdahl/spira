9e769a03fbb39e2e4218be6a45f59053f2fbc0ec
peter <57894152+studentCNRpXLG3@users.noreply.github.com>
2020-03-10 10:45:44 +0100

    UI.py: Refactor
    Conform to PEP 8: <https://www.python.org/dev/peps/pep-0008/>.

--- UI.py
+++ UI.py
@@ -10,42 +10,35 @@ def main():
 	cmd = [fp_ceptre, fp_game]
 	p = subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1)
 	for line in iter(p.stdout.readline, ""):
-
 		# For removing the starting rows
-		if(line == "#trace ...\n") :
+		if line == "#trace ...\n":
 			is_start = False
-
 		# Removes the ?- line
-		elif(re.match(r'\?-',line)) :
+		elif re.match(r'\?-',line):
 			print(line.rstrip()+"\n")
-
 		# Checks for the winner and print the name + won
-		elif(re.match(r'\{qui',line)):
+		elif re.match(r'\{qui',line):
 			print(re.search(r"(?<=\bwin\s)(\w+)", line).group() + " won\n")
 			print(line)
 			is_finished = True
-
 		# changes the (s (s (s z)))
-		elif(re.match(r'\d*: ',line)):
+		elif re.match(r'\d*: ',line):
 			print(modify(line).rstrip())
-
 		# Prints the line as it is but remove start and end
-		elif( dont_remove(line) and not is_start and not is_finished) : 
+		elif keep(line) and not is_start and not is_finished:
 			print(line.rstrip())
-			
 	p.stdout.close()
 	p.wait()
 
-# Modifies the (s (s z)) -> int
+# Modifies the (s (s z)) -> int.
 def modify(line):
 	tmp = re.sub(r"\([sz].*?\)", lambda m: str(m.group().count("s")), line)
-	return tmp.replace(")", "").replace(" z"," 0")
+	return tmp.replace(r')', "").replace(r'\sz'," 0")
 
-# Where we add everything case we wont write out the line
-def dont_remove(line):
-	if (re.match(r"Trace:",line)):
-		return False
-	else : 
+# Add every case we don't write out.
+def keep(line):
+	if re.match(r"Trace:",line):
 		return True
+    return False
 
 main()
