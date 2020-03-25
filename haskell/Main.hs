0190ccf64a862150bec2b546af467163a162727a
ErikLjungdahl <ekke.ekke@gmail.com>
2020-03-25 11:31:02 +0100

    Horse move for chess

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -2,16 +2,17 @@ import Game
 import Control.Monad.State
 
 import Prelude hiding ((+))
+import Data.List
 
 main :: IO ()
-main = runGame connectFour "game.cep"
+main = runGame chess "game.cep"
 
 ticTacToe :: M ()
 ticTacToe = do
     --nats
     --players ["jennie","simon","erik","peter","nicke","oskar"]
     (nat,s,z) <- gets nats
-    (player, playernames, stage_next_player) <- players ["simon","jennie","erik"]
+    (player, playernames, stage_next_player, opp) <- players ["simon","jennie","erik"]
 
     free <- newPredWithType "free" [nat,nat]
     occupied <- newPredWithType "occupied" [player,nat,nat]
@@ -24,9 +25,9 @@ ticTacToe = do
     stage_play<- stage "play" True [impl] p
 
     -- A player wins if they have 3 occupied tiles in a row/colum/diagnal
-    rowrule <- inARow    3 occupied p
-    colrule <- inAColumn 3 occupied p
-    diarules <- inADiagonal 3 occupied p
+    rowrule <- inARow    5 occupied p
+    colrule <- inAColumn 5 occupied p
+    diarules <- inADiagonal 5 occupied p
     stage_win <- stage "win" False (rowrule:colrule:diarules) p
 
     -- After play we check win condition
@@ -41,7 +42,7 @@ ticTacToe = do
 
     -- Set all tiles to free as initial state
     addAppliedPredsToInit $
-        map (applyPred free) [[applyVarTimes s z x ,applyVarTimes s z y] | x <- [0..2], y <- [0..2]]
+        map (applyPred free) [[applyVarTimes s z x ,applyVarTimes s z y] | x <- [0..19], y <- [0..19]]
     return ()
 
 
@@ -49,15 +50,15 @@ connectFour :: M ()
 connectFour = do
     --nats
     --players ["jennie","simon","erik","peter","nicke","oskar"]
-    (nat,s,zero) <- gets nats
-    (player, playernames, stage_next_player) <- players ["hugo","musen"]
+    (nat,suc,zero) <- gets nats
+    (player, playernames, stage_next_player, opp) <- players ["hugo","musen"]
 
     free <- newPredWithType "free" [nat,nat]
     occupied <- newPredWithType "occupied" [player,nat,nat]
 
     lt <- initLT
     maxFact  <- newFactType "max" [nat]
-    six <- zero+6
+    six <- zero+6 -- applyVarTimes s zero 6
     newFact maxFact [six]
 
     -- Pick a free tile and make it occupied by the player
@@ -71,7 +72,7 @@ connectFour = do
                , applyPred lt [y, m]
                ] -*
                [ applyPred occupied [p,x,y]
-               , applyPred free [x,yP1]
+               , applyPred free [x,yP1]  -- Makes the tile above free
                ]
     stage_play<- stage "play" True [impl] p
 
@@ -96,4 +97,92 @@ connectFour = do
     xs <- mapM (zero+) [0..6]
     addAppliedPredsToInit $
         map (\x -> applyPred free [x, zero]) xs
+    return ()
+
+chess :: M ()
+chess = do
+{-
+piece :  type.
+Häst  : piece.
+Bonde : piece.
+
+nothing : player.
+opp kalle nothing : player.
+opp kalle pelle : player.
+
+tile player piece nat nat : pred.
+
+stage move = {
+move/1
+    : tile P Häst X Y
+    -* tile P Häst (s X) (s (s Y))
+move/1
+    : tile P       Häst       X      Y
+    * opp P P2
+    * tile P2      _    (s (s X)) (s Y)
+    -o tile P      Häst (s (s X)) (s Y)
+    *  tile nothing free X Y
+-}
+
+
+    (nat,suc,zero) <- gets nats
+    (player, playernames, stage_next_player, opp) <- players ["hugo","musen"]
+    piece <- newType "piece"
+    horse <- newEmptyConstructor "horse" piece
+    nothing  <- newEmptyConstructor "nothing"  piece
+    tile <- newPredWithType "tile" [player, piece, nat, nat]
+
+    let free = head playernames
+    x <- newBinding nat
+    y <- newBinding nat
+    p <- newBinding player
+    p2 <- newBinding player
+    whatever <- newBinding piece
+
+    let horseImpl (rowB, rowA, colB, colA) =
+                [ applyPred opp [p, p2]
+                , applyPred tile [p2, whatever, rowA, colA]
+                , applyPred tile [p, horse, rowB ,colB]
+                ] -*
+                [ applyPred tile [p  , horse, rowA, colA]
+                , applyPred tile [free, nothing, rowB ,colB]
+                ]
+    let coordinates =
+            [(0,1,0,2)
+            ,(0,1,2,0)
+            ,(0,2,0,1)
+            ,(0,2,1,0)
+            ,(1,0,0,2)
+            ,(1,0,2,0)
+            ,(2,0,0,1)
+            ,(2,0,1,0)
+            ]
+    appliedBindings <- mapM (\(rowB,rowA,colB,colA) -> do
+        r1<-x+rowB
+        r2<-x+rowA
+        r3<-y+colB
+        r4<-y+colA
+        return (r1,r2,r3,r4)
+        ) coordinates
+    let impls = map horseImpl appliedBindings
+    stage_play <- stage "play" True impls p
+
+
+    -- After play we check win condition
+    stage_play `fromStageToStage` stage_play
+
+    let player1 = last playernames
+    initialStageAndPlayer stage_play (player1)
+
+    -- Set all tiles to free as initial state
+    --
+    xys <- mapM (\(x,y) -> do
+        x'<- zero+x
+        y'<- zero+y
+        return (x',y')
+        ) [(x,y) | x <-[0..7], y <- [0..7]]
+    addAppliedPredsToInit $
+        map (\(x,y) -> applyPred tile [free, nothing, x ,y]) xys
+    three <- zero+3
+    addAppliedPredsToInit [(applyPred tile [player1, horse,three,three])]
     return ()
